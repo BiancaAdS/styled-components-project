acdbfbdedcd4e7c80f5716344081d7f2e8b80d85
BiancaAdS <42790720+BiancaAdS@users.noreply.github.com>
2023-02-12 12:11:18 -0300

    Refatoração PasswordInput

---
+++ src/components/common/PasswordInput/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { PasswordInputStyled, PasswordInputWrapper, ToggleButton } from "./style";
+
+
+
+
+export const PasswordInput = (props) => {
+
+    const [showPassword, setShowPassword] = useState(true) 
+
+    return (
+        <>
+            <PasswordInputWrapper>
+                <PasswordInputStyled {...props}/>
+                <ToggleButton onClick={() => setShowPassword(original => !original)}>{showPassword ? 'Hide' :  'Show'}</ToggleButton>
+            </PasswordInputWrapper>
+            <div>
+                {showPassword ? props.value : ''}
+            </div>
+        </>
+    )
+}
